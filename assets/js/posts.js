d2588bed729a69fe475c8928eb922dae68ecffcf
EeneeS <soeteniels1803@gmail.com>
2024-05-01 22:00:58 +0200

    doing some things im not so proud of...

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,7 +1,22 @@
 export const POSTS = [
     {
         title: "Guest lecture Combell",
-        content: "short description",
+        content: `
+            During a guest lecture organized by a fellow student, Simon Verdonck 
+            unfolded the compelling narrative of Combell's origins and its evolution into an indispensable force
+            within the hosting industry. <br/> <br/>
+            At the heart of Combell's premium hosting lies a sophisticated infrastructure intelligently distributing 
+            computing power across multiple servers. In the event of one server's failure, another seamlessly takes over. <br/> <br/>
+            Significant investments are dedicated to state-of-the-art data centers, boasting not only redundant data storage but 
+            also power redundancy from various sources and backed by battery support. Dual internet providers collaborate to minimize any 
+            potential downtime. <br/> <br/>
+            Combell envisions next-generation hosting. With Linux Debian, Apache web hosting, and individual containers fortified 
+            with firewalls for customers, they go beyond simple storage solutions. Security ascends to new heights, incorporating features 
+            like SSH access and brute force protection. <br/> <br/>
+            Did you know? Even the servers handling the ticket sales of Tomorrowland, one of the world's 
+            largest events, find their home at Combell. <br/> <br/>
+            A heartfelt thank you to Simon Verdonck for this insightful lecture!
+`,
         img_url: "/images/combell-logo.png",
         post_url: "combell",
     },
